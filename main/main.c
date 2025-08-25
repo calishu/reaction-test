ebdcd3faa4a2ba8d9e120a73eeac0ea9d7197622
calishu <danny.kretschmann@proton.me>
2025-08-26 01:34:11 +0200

    feat: added main loop

--- main/main.c
+++ main/main.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
+#include <stdint.h>
 
 #include "led.h"
 #include "rng.h"
 
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "esp_timer.h"
+
+#define TAG "Reaction Test"
 
 #define RED_LED GPIO_NUM_6
 #define GREEN_LED GPIO_NUM_5
-#define BUTTON GPIO_NUM_3
+#define BUTTON GPIO_NUM_7
 #define STOP_BUTTON GPIO_NUM_4
 
 gpio_config_t get_config(gpio_num_t gpio_num)
@@ -22,6 +25,32 @@ gpio_config_t get_config(gpio_num_t gpio_num)
     return io_conf;
 }
 
+void loop()
+{
+    bool stop_action = false;
+
+    while (stop_action != true)
+    {
+        gpio_set_level(GREEN_LED, 0);
+        gpio_set_level(RED_LED, 0);
+
+        uint8_t stop_button_state = gpio_get_level(STOP_BUTTON);
+        if (stop_button_state == 1) stop_action = true;
+
+        gpio_set_level(RED_LED, 1);
+        wait(get_random_number(3000, 6000));
+        gpio_set_level(RED_LED, 0);
+
+        int64_t start = esp_timer_get_time();
+        gpio_set_level(GREEN_LED, 1);
+
+        while (gpio_get_level(BUTTON) == 0 || (start * 1000) < 2500)
+            __asm__("nop");
+
+        ESP_LOGI(TAG, "You took a long time ig");
+    }
+}
+
 void app_main()
 {
     gpio_config_t red_led     = get_config(RED_LED);
@@ -33,4 +62,6 @@ void app_main()
     gpio_config(&green_led);
     gpio_config(&button);
     gpio_config(&stop_button);
+
+    loop();
 }
